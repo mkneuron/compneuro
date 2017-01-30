9bc8160ef218c89c398da199ee61fa7fc9c27af1
mkneuron <mkneuro@protonmail.com>
2017-01-30 14:49:36 +0200

    Update and rename actor to actor.m

--- finalAssignments/mouseTorturer/actor.m
+++ finalAssignments/mouseTorturer/actor.m
@@ -16,7 +16,7 @@ actor_cell_activity= z_ij'*spikeCounts;
 
 for j = 1:act_Cells
     P_movement_direction(j) = (exp(2*actor_cell_activity(j)))./...
-                                 (sum(exp(actor_cell_activity)));
+                                 (sum(exp(2*actor_cell_activity)));
 end
 if sum(P_movement_direction) ~= 1
     warning('P_movement_direction is not normalized');
@@ -26,14 +26,14 @@ end
 % Scroll the probabilities, if a random number is between a probability
 % value p1 and p1 + the next one, then chose the corresponding action. 
 random_selector = rand();
-lower_bound = 0;
 direction = zeros(1, act_Cells);
+cumulative = cumsum(P_movement_direction); % cumulative probabilities of action
+
 for j = 1:act_Cells
-    if (random_selector <= (P_movement_direction(1, j) + lower_bound)) && ...
-                                       (random_selector > lower_bound)
+    if (random_selector <= cumulative(j))
         direction(1, j) = 1;
+        break
     end
-    lower_bound = P_movement_direction(1, j);
 end
  
 %% Update weights given the prediction error from the Critic.
@@ -47,4 +47,8 @@ end
 %         z_ij_dx(i, j) = pred_err.*z_ij(i,j).*direction(1, j);
 %     end
 % end
+movement_tmp = find(direction);
+if ~isscalar(movement_tmp)
+    warning('Dumb rat (Actor) chose 2 movement directions!')
+end
 end
